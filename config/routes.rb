6e67e492105464395ceae867ca549d562842620b
rikuan <kuriudona2812@gmail.com>
2020-07-19 20:59:24 +0900

    manegement-users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'groups#index'
   resources :users, only: [:edit, :update]
-  root "messages#index"
+  resources :groups, only: [:index, :new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
+  
 
+  # root "messages#index"
+  
 end
